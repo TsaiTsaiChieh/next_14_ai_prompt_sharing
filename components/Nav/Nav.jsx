90faa4cf90d7948a1cef25370aeb67a6cb42a686
Tsai-Chieh <d0177381@gmail.com>
2024-10-03 15:13:14 +0800

    :sparkles: add the nav and its style

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -1,10 +1,130 @@
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './Nav.module.css';
+import { getProviders, signIn, signOut } from 'next-auth/react';
+import { useState, useEffect } from 'react';
 
 const Nav = () => {
+  const isUserLoggedIn = false;
+  const [provider, setProvider] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+  const offDropdown = () => setToggleDropdown(false);
+
+  useEffect(() => {
+    const setupProviders = async () => {
+      const res = await getProviders();
+      setProvider(res);
+    };
+    setupProviders();
+  }, []);
   return (
-    <div>
-      Nav
-    </div>
+    <nav className={`flex-between ${styles.nav}`}>
+      <Link href='/' className={`flex-center ${styles.nav__logoContainer}`}>
+        <Image
+          src='/assets/images/logo.svg'
+          className={styles.nav__logoImage}
+          alt='Promptopia Logo'
+          width={30}
+          height={30}
+          onClick={offDropdown}
+        ></Image>
+        <p className='logo_text'>Promptopia</p>
+      </Link>
+
+      {/* Desktop Navigation */}
+      <div className={styles.nav__desktopContainer}>
+        {isUserLoggedIn ? (
+          <div className={styles.nav__userActions}>
+            <Link href='create-prompt' className='black_btn'>
+              Create Prompt
+            </Link>
+            <button type='button' className='outline_btn' onClick={signOut}>
+              Sign Out
+            </button>
+            <Link href='/profile'>
+              <Image
+                className={styles.nav__profileImage}
+                src='/assets/images/logo.svg'
+                alt='profile'
+                width={37}
+                height={37}
+              ></Image>
+            </Link>
+          </div>
+        ) : (
+          <>
+            {provider &&
+              Object.values(provider).map(provider => (
+                <button
+                  type='button'
+                  key={provider}
+                  className='black_btn'
+                  onClick={() => signIn(provider.id)}
+                >
+                  {`Sign In with ${provider.name}`}
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className={styles.nav__mobileContainer}>
+        {isUserLoggedIn ? (
+          <div className={styles.nav__mobileUser}>
+            <Image
+              className={styles.nav__profileImage}
+              src='/assets/images/logo.svg'
+              alt='profile'
+              width={37}
+              height={37}
+              onClick={() => setToggleDropdown(prev => !prev)}
+            ></Image>
+            {toggleDropdown && (
+              <div className={styles.nav__dropdown}>
+                <Link
+                  href='/profile'
+                  className={styles.nav__dropdownLink}
+                  onClick={offDropdown}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href='/create-prompt'
+                  className={styles.nav__dropdownLink}
+                  onClick={offDropdown}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type='button'
+                  className='black_btn'
+                  onClick={offDropdown && signOut}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {provider &&
+              Object.values(provider).map(provider => (
+                <button
+                  type='button'
+                  key={provider}
+                  className={`black_btn ${styles.nav__mobileSignOut}`}
+                  onClick={() => signIn(provider.id)}
+                >
+                  {`Sign In with ${provider.name}`}
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
   );
-}
+};
 
 export default Nav;
