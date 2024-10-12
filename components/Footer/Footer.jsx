7b0d553db60eeb38a9478d9250ade1932fe5adec
Tsai-Chieh <d0177381@gmail.com>
2024-10-13 02:59:58 +0800

    :art: format the date

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,9 +1,17 @@
 import styles from "./Footer.module.css";
 
 const Footer = () => {
+  const formatDate = isoDateString => {
+    const offsetHours = new Date(isoDateString).getTimezoneOffset() / -60;
+    const formattedOffset = `${offsetHours >= 0 ? "+" : ""}${Math.abs(
+      offsetHours
+    )}`;
+  };
   return (
     <footer className={styles.footer}>
-      <span>{`Deploy Date: ${process.env.NEXT_PUBLIC_BUILD_DATE}`}</span>
+      <span>{`Deploy Date: ${formatDate(
+        process.env.NEXT_PUBLIC_BUILD_DATE
+      )}`}</span>
       <br />
       <span>{`Deploy branch: ${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}`}</span>
     </footer>
