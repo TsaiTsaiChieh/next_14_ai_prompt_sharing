64602656f44b7ce452c8380b25d8e94e4f3fdba5
Tsai-Chieh <d0177381@gmail.com>
2024-10-12 22:41:55 +0800

    :sparkles: finish the promptCard component and its API

--- components/PromptCard/PromptCard.jsx
+++ components/PromptCard/PromptCard.jsx
@@ -1,5 +1,61 @@
-const PromptCard = ({ data, handleTagClick }) => {
-  return <div>PromptCard</div>;
+"use client";
+import Image from "next/image";
+import styles from "./PromptCard.module.css";
+import { useState } from "react";
+
+const PromptCard = ({ post, handleTagClick }) => {
+  const [copied, setCopied] = useState("");
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => {
+      setCopied("");
+    }, 3000);
+  };
+  return (
+    <div className={styles.promptCard}>
+      <div className={styles.promptCard__header}>
+        <div className={styles.promptCard__profile}>
+          <Image
+            className={styles.promptCard__profileImage}
+            src={post.creator.image}
+            alt='user_image'
+            width={40}
+            height={40}
+          ></Image>
+          <div className={styles.promptCard__info}>
+            <h3 className={styles.promptCard__username}>
+              {post.creator.username}
+            </h3>
+            <p className={styles.promptCard__email}>{post.creator.email}</p>
+          </div>
+        </div>
+
+        <div className='copy_btn' onClick={() => {}}>
+          <Image
+            src={
+              copied === post.prompt
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
+            }
+            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
+            width={12}
+            height={12}
+            onClick={handleCopy}
+          ></Image>
+        </div>
+      </div>
+
+      <p className={styles.promptCard__content}>{post.prompt}</p>
+      <p
+        className={`blue_gradient ${styles.promptCard__tag}`}
+        onClick={handleTagClick && handleTagClick(post.tag)}
+      >
+        #{post.tag}
+      </p>
+    </div>
+  );
 };
 
 export default PromptCard;
