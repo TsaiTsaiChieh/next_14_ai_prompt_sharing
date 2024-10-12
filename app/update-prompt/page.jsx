b20a30bd880edb29f3613a207146b0d37f3aa441
Tsai-Chieh <d0177381@gmail.com>
2024-10-13 02:40:18 +0800

    :bug: fix the suspense issue, ref from https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,54 +1,61 @@
 "use client";
 import Form from "@components/Form/Form";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 const EditPrompt = () => {
-  // const router = useRouter();
-  // const searchParams = useSearchParams();
-  // const promptId = searchParams.get("id");
-  // const [submitting, setSubmitting] = useState(false);
-  // const [post, setPost] = useState({
-  //   prompt: "",
-  //   tag: "",
-  // });
-  // useEffect(() => {
-  //   const getPrompt = async () => {
-  //     const res = await fetch(`/api/prompt/${promptId}`);
-  //     setPost(await res.json());
-  //   };
-  //   if (promptId) getPrompt();
-  // }, [promptId]);
-  // const updatePrompt = async e => {
-  //   e.preventDefault();
-  //   setSubmitting(true);
-  //   if (!promptId) return alert("Missing PromptId!");
-  //   try {
-  //     const res = await fetch(`/api/prompt/${promptId}`, {
-  //       method: "PATCH",
-  //       body: JSON.stringify({
-  //         prompt: post.prompt,
-  //         tag: post.tag,
-  //       }),
-  //     });
-  //     if (res.ok) {
-  //       router.push("/");
-  //     }
-  //   } catch (error) {
-  //     console.error(error);
-  //   } finally {
-  //     setSubmitting(false);
-  //   }
-  // };
-  // return (
-  //   <Form
-  //     type='Edit'
-  //     post={post}
-  //     setPost={setPost}
-  //     submitting={submitting}
-  //     handleSubmit={updatePrompt}
-  //   />
-  // );
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+  useEffect(() => {
+    const getPrompt = async () => {
+      const res = await fetch(`/api/prompt/${promptId}`);
+      setPost(await res.json());
+    };
+    if (promptId) getPrompt();
+  }, [promptId]);
+  const updatePrompt = async e => {
+    e.preventDefault();
+    setSubmitting(true);
+    if (!promptId) return alert("Missing PromptId!");
+    try {
+      const res = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+      if (res.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+  return (
+    <Form
+      type='Edit'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
+  );
 };
 
-export default EditPrompt;
+const EditPromptWrapper = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <EditPrompt />
+  </Suspense>
+);
+
+export default EditPromptWrapper;
+// export default EditPrompt;
